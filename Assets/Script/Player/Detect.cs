06af9c9235a4b2abfe79a10226d50faccb0ecf47
PIpi123213 <1455076726@qq.com>
2024-07-07 02:37:52 +0800

    237

--- Assets/Script/Player/Detect.cs
+++ Assets/Script/Player/Detect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,7 +10,11 @@ public class Detect : MonoBehaviour
    
     // 要检测的图层
     public LayerMask layerMask;
+    public LayerMask layerMaskFruit;
+
+
     private GameObject hitObject;
+    private GameObject hitObject1;
     public GameObject phone;
     // public GameObject canvas;
 
@@ -28,6 +33,27 @@ public class Detect : MonoBehaviour
 
     private GameObject canvas;
 
+    public enum Item
+    {
+        Potato1,
+        Tomato1,
+        Cabbage1,
+        Garlic1,
+        Onion1,
+        Apple1,
+        Watermelon1,
+        Orange1,
+        Grapes1,
+        Strawberry1,
+        sum,
+        Count // 总数
+    }
+    private int[] itemQuantities;
+    private float moveDistance = 0.2f; // 上升的距离
+    private float moveDuration = 0.3f; // 上升的持续时间
+
+    private Vector3 originalPosition;
+    private bool isMoving = false;
     void Start()
     {
         
@@ -36,6 +62,7 @@ public class Detect : MonoBehaviour
         isfind.SetActive(false);
         isDecting = false;
         isShow = false;
+        Inventory();
     }
 
 
@@ -65,12 +92,13 @@ public class Detect : MonoBehaviour
 
 
         }
+        DetectObjectUnderMouseFruit();
 
 
 
 
 
-       
+
 
         if (Input.GetMouseButtonDown(1)&&!isShow)
         {
@@ -139,6 +167,12 @@ public class Detect : MonoBehaviour
             }
             else
             {
+                if (hitObject != null)
+                {
+                    canvas = null;
+                    hitObject = null;
+
+                }
                 isscanning.SetActive(true);
                 isfind.SetActive(false);
             }
@@ -162,6 +196,139 @@ public class Detect : MonoBehaviour
             }
         }
     }
+    IEnumerator MoveObject(GameObject obj)
+    {
+        isMoving = true;
+
+        Vector3 originalPosition = obj.transform.position;
+        Vector3 targetPosition = originalPosition + Vector3.up * moveDistance;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            float t = elapsedTime / moveDuration;
+            obj.transform.position = Vector3.Lerp(originalPosition, targetPosition, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.2f); // 等待一段时间
+
+        elapsedTime = 0f;
+        while (elapsedTime < moveDuration)
+        {
+            float t = elapsedTime / moveDuration;
+            obj.transform.position = Vector3.Lerp(targetPosition, originalPosition, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        obj.transform.position = originalPosition; // 确保回到原始位置
+        isMoving = false;
+    }
+
+    void DetectObjectUnderMouseFruit()
+    {
+        // 从摄像机发射一条射线到鼠标位置
+        Vector3 viewportCenter = new Vector3(0.5f, 0.5f, 0f);
+        Ray ray = Camera.main.ViewportPointToRay(viewportCenter);
+        RaycastHit hit;
+       
+
+        // 如果射线碰到某个物体，并且该物体在指定的图层中
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMaskFruit))
+        {
+            // 获取碰到的物体
+            hitObject1 = hit.collider.gameObject;
+            InterObject _interObject = hitObject1.GetComponent<InterObject>();
+            float hitDistance = hit.distance;
+            //canvas = _interObject.canvas;
+            //Debug.Log("Hit distance: " + hitDistance);
+            if (hitDistance <= maxDetectionDistance/2.0f)
+            {
+                Debug.Log("1" );
+                if(Input.GetKeyDown(KeyCode.E) && _interObject.name== "bingxiang" && itemQuantities[10] != 0)
+                {
+                    if (itemQuantities[0]==3)
+                    {
+                        Gamewin();
+
+
+
+
+                    }
+                    else
+                    {
+                        Gamelose();
+                    }
+
+
+                }
+
+
+
+
+                if (Input.GetKeyDown(KeyCode.E) && itemQuantities[10] < 3)
+                {
+                    Debug.Log(_interObject.name);
+
+                    if (Enum.TryParse(_interObject.name, out Item item))
+                    {
+                        Debug.Log("3");
+                        StartCoroutine(MoveObject(hitObject1));
+
+                        itemQuantities[(int)item]++;
+                        itemQuantities[10]++;
+                        Debug.Log($"{_interObject.name} detected. Quantity: {itemQuantities[(int)item]}");
+                    }
+
+                }
+
+            }
+            else
+            {
+                if (hitObject1 != null)
+                {
+
+                    hitObject1 = null;
+
+                }
+            }
+            if (_interObject == null)
+            {
+                //Debug.Log("no");
+            }
+            // 检查物体的标签是否是目标标签
+
+        }
+        else
+        {
+           
+            // 获取所有目标标签的物体并调用 OnPointerExit
+            if (hitObject1 != null)
+            {
+                
+                hitObject1 = null;
+
+            }
+        }
+    }
+    public void Inventory()
+    {
+        itemQuantities = new int[(int)Item.Count];
+
+        // 初始化每个物品的数量为0
+        for (int i = 0; i < itemQuantities.Length; i++)
+        {
+            itemQuantities[i] = 0;
+        }
+    }
+
+
+
 
     bool IsObjectInScanZone(GameObject obj)
     {
@@ -204,8 +371,20 @@ public class Detect : MonoBehaviour
         return true; // 所有顶点都在扫描区内
     }
 
+    public void Gamewin()
+    {
+        Debug.Log("win");
 
 
+
+    }
+    public void Gamelose()
+    {
+        Debug.Log("lose");
+
+
+
+    }
 
 
 
